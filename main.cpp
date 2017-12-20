c6f5960d99edb128378bd000a529a52e4ffd558d
Carmelo Evoli <carmelo.evoli@me.com>
2017-12-20 10:51:31 +0100

    active evolve

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 int main() {
 	Waves * W = new Waves();
 
+	std::cout << " ... init\n";
+
 	W->build_z_axis(100 * pc, 2001);
 	W->build_p_axis(1e2 * GeV_c, 1e6 * GeV_c, 5 * 4); // 32);
 	W->build_CR_source_term();
@@ -14,19 +16,17 @@ int main() {
 	W->build_W_sg();
 	W->build_f_cr();
 	W->build_D_zz();
-
 	W->build_v_A();
 	W->build_energy_losses();
 
-	std::cout << "dump initial state ... \n";
 	W->dump(0);
 	W->print_status(0, time(NULL));
 
-	//cout << "evolve ... \n";
+	std::cout << " ... evolve\n";
 
-	//W->evolve();
+	W->evolve(0.1 * year, 340 * 10000, 10000);
 
-	//cout << "... done! \n";
+	std::cout << "... done! \n";
 
 	delete W;
 	return 0;
