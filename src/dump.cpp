42d536c2fa99b672c89a927d591532ffa43a2b8f
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-07-02 17:08:16 +0200

    fixed output header

--- src/dump.cpp
+++ src/dump.cpp
@@ -15,7 +15,8 @@ void Waves::dump(const double& t) {
   std::string filename = generate_output_filename("fcr", t);
   std::cout << "dumping f_cr on this file: " << filename << " ... ";
   std::ofstream outfile(filename.c_str());
-  outfile << "# z [kpc]  p [GeV/c]  fcr []  Dzz []  dfdz []  flux [] \n";
+  outfile << "# z[pc]  p[GeV/c]  fcr[GeV^-3 m^-3]  Dzz[cm^2/s]  dfdz[GeV^-3 m^-3 kpc^-1]  diff.flux[GeV^-3 "
+             "m^-3 s^-1] W_sg[mks] Q_cr[mks]\n";
   outfile << std::scientific << std::setprecision(4);
   for (size_t iz = 0; iz < z.get_size(); ++iz)
     for (size_t ip = 0; ip < p.get_size(); ++ip) {
@@ -38,7 +39,7 @@ void Waves::dump_analytical_test(const double& t) {
   std::string filename = generate_output_filename("test", t);
   std::cout << "dumping analytical solution on this file: " << filename << " ... ";
   std::ofstream outfile(filename.c_str());
-  outfile << "# z [kpc]  p [GeV/c]  fcr []  fcr_a [] \n";
+  outfile << "# z[pc]  p[GeV/c]  fcr[GeV^-3 m^-3]  fcr_a[GeV^-3 m^-3] \n";
   outfile << std::scientific << std::setprecision(4);
   double units = (1. / pow3(mks::GeV_c) / pow3(mks::meter));
   for (size_t iz = 0; iz < z.get_size(); ++iz)
@@ -51,12 +52,6 @@ void Waves::dump_analytical_test(const double& t) {
     }
   outfile.close();
   std::cout << "... done!\n";
-}
-
-void Waves::dump_rates(const double& t) {
-  std::string filename = generate_output_filename("rates", t);
-  std::cout << "dumping rates on this file: " << filename << " ... ";
-  std::ofstream outfile(filename.c_str());
 }
 
 }  // namespace CRWAVES
