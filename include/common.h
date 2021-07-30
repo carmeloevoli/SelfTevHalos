997cc4d6c095da464ad50f67b898c61f90ae922c
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-07-30 14:04:51 +0200

    inline function

--- include/common.h
+++ include/common.h
@@ -2,16 +2,30 @@
 #ifndef UTILITIES_H_
 #define UTILITIES_H_
 
+#include <utilities/misc.h>
+
+#include <cmath>
+
+#include "units.h"
+
 namespace CRWAVES {
 
-double beta(const double& p);
-double larmor_radius(const double& p, const double& B);
-double inverse_larmor_radius(const double& k, const double& B);
+inline double larmor_radius(double p, double B) { return p / mks::elementary_charge / B; }
+inline double inverse_larmor_radius(double k, double B) { return mks::elementary_charge * B / k; }
+inline double source_evolution(double t, double t_decay) { return 1. / utils::pow_integer<2>(1. + t / t_decay); }
+inline double gamma2(double p) { return 1. + utils::pow_integer<2>(p / mks::electron_mass_c); }
+inline double beta(double p) { return std::sqrt(1. - 1. / gamma2(p)); }
+inline double gaussian_1D(double z, double size) {
+  return std::pow(2.0 * M_PI * utils::pow_integer<2>(size), -0.5) * std::exp(-0.5 * utils::pow_integer<2>(z / size));
+}
+inline double gaussian_3D(double z, double size) {
+  return std::pow(2.0 * M_PI * utils::pow_integer<2>(size), -1.5) * std::exp(-0.5 * utils::pow_integer<2>(z / size));
+}
+inline double pl_cutoff(const double& p, const double& alpha, const double& p_min, const double& p_cutoff) {
+  return (p > p_min) ? std::pow(p / mks::electron_mass_c, -alpha) * std::exp(-utils::pow_integer<2>(p / p_cutoff)) : 0.;
+}
+
 double I_of_alpha(const double& alpha, const double& x_min, const double& x_cutoff);
-double source_evolution(const double& t_now, const double& t_decay);
-double source_profile_1D(const double& z, const double& size);
-double source_profile_3D(const double& z, const double& size);
-double source_spectrum(const double& p, const double& alpha, const double& p_min, const double& p_cutoff);
 
 }  // namespace CRWAVES
 
--- src/common.cpp
+++ src/common.cpp
@@ -2,23 +2,8 @@
 #include "common.h"
 
 #include <gsl/gsl_integration.h>
-#include <utilities/misc.h>
-
-#include <cmath>
-
-#include "units.h"
 
 namespace CRWAVES {
-
-double beta(const double& p) {
-  using std::sqrt;
-  const double gamma2 = 1. + utils::pow_integer<2>(p / mks::electron_mass_c);
-  return sqrt(1. - 1. / gamma2);
-}
-
-double larmor_radius(const double& p, const double& B) { return p / mks::elementary_charge / B; }
-
-double inverse_larmor_radius(const double& k, const double& B) { return mks::elementary_charge * B / k; }
 
 double f(double x, void* params) {
   double alpha = *(double*)params;
@@ -38,26 +23,6 @@ double I_of_alpha(const double& alpha, const double& x_min, const double& x_cuto
   gsl_integration_qag(&F, x_min, 2.0 * x_cutoff, 0, 1e-3, LIMIT, 3, w, &result, &error);
   gsl_integration_workspace_free(w);
   return result;
-}
-
-double source_evolution(const double& t_now, const double& t_decay) { return std::pow(1. + t_now / t_decay, -2.); }
-
-double source_profile_1D(const double& z, const double& size) {
-  using std::exp;
-  using std::pow;
-  return pow(2.0 * M_PI * utils::pow_integer<2>(size), -1. / 2.) * exp(-0.5 * utils::pow_integer<2>(z / size));
-}
-
-double source_profile_3D(const double& z, const double& size) {
-  using std::exp;
-  using std::pow;
-  return pow(2.0 * M_PI * utils::pow_integer<2>(size), -3. / 2.) * exp(-0.5 * utils::pow_integer<2>(z / size));
-}
-
-double source_spectrum(const double& p, const double& alpha, const double& p_min, const double& p_cutoff) {
-  using std::exp;
-  using std::pow;
-  return (p > p_min) ? pow(p / mks::electron_mass_c, -alpha) * exp(-utils::pow_integer<2>(p / p_cutoff)) : 0.;
 }
 
 }  // namespace CRWAVES
